233b05c9b884b99738fa06534c8a2e2b5b6cc2d8
Ruchidnya <ruchidnya@gmail.com>
2020-07-09 19:15:13 +0530

    -

--- Factory_method.cpp
+++ Factory_method.cpp
@@ -29,13 +29,48 @@ class Whale : public Animal
 		}
 };
 
+class User
+{
+	public:
+	User(int t)
+	{
+		if(t==1)
+		{
+			anim = new Dog();
+		}
+		else if(t==2)
+		{
+			anim = new Whale();
+		}
+		else
+		{
+			anim=NULL;
+		}
+	}
+	~User()
+	{
+		if(anim)
+		{
+			delete[] anim;
+			anim = NULL;
+		}
+	}
+
+	Animal *getAnimal()
+	{
+		return anim;
+	}
+	private:
+	Animal *anim;
+};
+
 int main()
 {
-	Animal *a= new Animal();
-	Animal *d= new Dog();
-	Animal *w=new Whale();
-
-	a->Move();
+	User *u1 = new User(1);
+	User *u2 = new User(2);
+	Animal *d= u1->getAnimal();
+	Animal *w= u2->getAnimal();
+	
 	d->Move();
 	w->Move();
 }
